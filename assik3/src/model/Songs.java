18de713e495f41fb7ebeed644bf16ae44b04bc2f
Alibek <52044412+Alibek15@users.noreply.github.com>
2023-05-03 19:38:20 +0600

    Update Songs.java

--- assik3/src/model/Songs.java
+++ assik3/src/model/Songs.java
@@ -5,6 +5,12 @@ public class Songs {
     private String name;
     private String artist;
     private Integer year;
+    
+    public Songs(final String name, final String artist, final Integer year) {
+        this.name = name;
+        this.artist = artist;
+        this.year = year;
+    }
 
     public String getName() {
         return name;
@@ -29,12 +35,26 @@ public class Songs {
     public void setYear(final Integer year) {
         this.year = year;
     }
-    @Override
+     @Override
     public int hashCode(){
         int hash=1;
         for (int i = 0; i <name.toCharArray().length ; i++) {
             hash= i+31*hash;
         }
+        for (int i = 0; i <artist.toCharArray().length ; i++) {
+            hash= i+31*hash;
+        }
+        hash+=year;
+
         return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Songs{" +
+                "name='" + name + '\'' +
+                ", artist='" + artist + '\'' +
+                ", year=" + year +
+                '}';
     }
 }
