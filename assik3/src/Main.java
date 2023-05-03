215526402ff5d9be7680531ef2b2b19afd627d77
Alibek <52044412+Alibek15@users.noreply.github.com>
2023-05-03 19:37:44 +0600

    Update Main.java

--- assik3/src/Main.java
+++ assik3/src/Main.java
@@ -6,12 +6,29 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         MyHashTable<Songs, String> table = new MyHashTable<>();
+         StringBuffer artist=new StringBuffer();
+        StringBuffer name=new StringBuffer();
+        StringBuffer album=new StringBuffer();
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
-            Songs name = new Songs();
-            Songs artist = new Songs();
-            Songs year = new Songs();
-
+            int year=random.nextInt(1900,2020);
+            for (int j = 0; j <20 ; j++) {
+                name.append(random.nextInt(65,90));
+            }
+            for (int j = 0; j <20 ; j++) {
+                artist.append(random.nextInt(65,90));
+            }
+            for (int j = 0; j <20 ; j++) {
+                album.append(random.nextInt(65,90));
+            }
+            table.put(new Songs(name.toString(), artist.toString(),year),album.toString());
         }
+        Songs val1=new Songs("After hours", "the Weekend", 2019 );
+        Songs val2=new Songs("Blinding lights", "the Weekend", 2021 );
+        System.out.println("Get " + table.get(val1));
+         table.put(val1, "after hours");
+        System.out.println("Contains " + table.contains("after hours"));
+        System.out.println("Get key " + table.getKey("after hours"));
+        System.out.println("remove " + table.remove(val2));
     }
 }
